420fe69fd49166a7c514414759a247207c205305
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-02-07 17:43:47 +0300

    Math Functions

--- 9_Math_Functions.c
+++ 9_Math_Functions.c
@@ -4,5 +4,34 @@
 int main(){
 
     //Square Root Function
-    double d = sqrt(9);
+    double A = sqrt(9);
+
+    //Raising a number to a power
+    double B = pow(2, 4);
+
+    //Rounding off a number
+    int C = round(3.14);
+
+    //Rounding a number up using ceil
+    int D = ceil(3.14);
+
+    //Rounding down number up using floor
+    int E = floor(3.14);
+
+    //Absolute value of a number using fabs ie how far away it is from 0
+    double F = fabs(-389);
+
+    //Logarithm of a number
+    double G = log(3);
+
+    //sin
+    double H = sin(45);
+
+    //cos
+    double I = cos(45);
+    
+    //tan
+    double J = tan(45);
+
+    printf("%lf", J);
 }
